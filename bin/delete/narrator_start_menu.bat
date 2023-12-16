51d31e63c774cde67bcf8914be5de0383d85daf8
clqwnless <132391463+clqwnless@users.noreply.github.com>
2023-12-15 17:35:34 -0800

    bug fix

--- bin/delete/narrator_start_menu.bat
+++ bin/delete/narrator_start_menu.bat
@@ -1,16 +1,14 @@
 @echo off
 
 
-:: %1 is mount_path
+set mount_path="%~1"
 
-if "%1" == "" (
+if %mount_path% == "" (
     echo.
     echo   [*ERROR] Mount path argument required
     echo.
     pause
     exit /b 1
 )
-
-set mount_path=%1
 
 del /s /q %mount_path:~0,-1%\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessibility\Narrator.lnk" > nul 2>&1
