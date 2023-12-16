55fce44c28dd24dd2c614d24330d6a16e9dc7119
clqwnless <132391463+clqwnless@users.noreply.github.com>
2023-12-15 17:37:54 -0800

    bug fix

--- bin/delete/speech_recognition_start_menu.bat
+++ bin/delete/speech_recognition_start_menu.bat
@@ -1,16 +1,14 @@
 @echo off
 
 
-:: %1 is mount_path
+set mount_path="%~1"
 
-if "%1" == "" (
+if %mount_path% == "" (
     echo.
     echo   [*ERROR] Mount path argument required
     echo.
     pause
     exit /b 1
 )
-
-set mount_path=%1
 
 del /s /q %mount_path:~0,-1%\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessibility\Speech Recognition.lnk" > nul 2>&1
