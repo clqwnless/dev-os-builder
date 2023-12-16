0c91ad5659d7cb54dc2e1855c0739df983110b91
clqwnless <132391463+clqwnless@users.noreply.github.com>
2023-12-15 17:39:46 -0800

    bug fix

--- bin/remove/netfx_3.bat
+++ bin/remove/netfx_3.bat
@@ -1,18 +1,15 @@
 @echo off
 
 
-:: %1 is mount_path
+set mount_path="%~1"
 
-
-if "%1" == "" (
+if %mount_path% == "" (
     echo.
     echo   [*ERROR] Mount path argument required
     echo.
     pause
     exit /b 1
 )
-
-set mount_path=%1
 
 dism /Image:%mount_path% /Disable-Feature /FeatureName:NetFx3 /Remove
 
