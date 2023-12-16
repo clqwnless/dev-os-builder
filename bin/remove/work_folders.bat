0d8f1e90567b650f430d66f2321192be020cec67
clqwnless <132391463+clqwnless@users.noreply.github.com>
2023-12-15 17:42:17 -0800

    bug fix

--- bin/remove/work_folders.bat
+++ bin/remove/work_folders.bat
@@ -1,19 +1,15 @@
 @echo off
 
 
-:: %1 is mount_path
+set mount_path="%~1"
+set feature_name=WorkFolders-Client
 
-
-if "%1" == "" (
+if %mount_path% == "" (
     echo.
     echo   [*ERROR] Mount path argument required
     echo.
     pause
     exit /b 1
 )
-
-set mount_path=%1
-
-set feature_name=WorkFolders-Client
 
 dism /Image:%mount_path% /Disable-Feature /FeatureName:%feature_name% /Remove
