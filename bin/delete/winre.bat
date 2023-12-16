8e06c33da05b51dc5f9ca572480dec96c530a7fe
clqwnless <132391463+clqwnless@users.noreply.github.com>
2023-12-15 17:38:09 -0800

    bug fix

--- bin/delete/winre.bat
+++ bin/delete/winre.bat
@@ -1,17 +1,14 @@
 @echo off
 
 
-:: %1 is mount_path
+set mount_path="%~1"
 
-if "%1" == "" (
+if %mount_path% == "" (
     echo.
     echo   [*ERROR] Mount path argument required
     echo.
     pause
     exit /b 1
 )
-
-
-set mount_path=%1
 
 rmdir /s /q %mount_path:~0,-1%\Windows\System32\Recovery" > nul 2>&1
