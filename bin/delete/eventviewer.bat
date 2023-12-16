968ccfd741fba8b54c25bf158b9555be11315f75
clqwnless <132391463+clqwnless@users.noreply.github.com>
2023-12-15 17:30:54 -0800

    bug fix

--- bin/delete/eventviewer.bat
+++ bin/delete/eventviewer.bat
@@ -1,9 +1,9 @@
 @echo off
 
 
-:: %1 is mount_path
+set mount_path="%~1"
 
-if "%1" == "" (
+if %mount_path% == "" (
     echo.
     echo   [*ERROR] Mount path argument required
     echo.
@@ -11,8 +11,6 @@ if "%1" == "" (
     exit /b 1
 )
 
-
-set mount_path=%1
 
 del /s /q %mount_path:~0,-1%\Windows\System32\eventvwr.exe" > nul 2>&1
 del /s /q %mount_path:~0,-1%\Windows\System32\eventvwr.msc" > nul 2>&1
