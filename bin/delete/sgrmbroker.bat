94a4daee4cd5a8eb1fa99cacf5f6d80d9c4dd315
clqwnless <132391463+clqwnless@users.noreply.github.com>
2023-12-23 05:35:46 -0800

    upd

---
+++ bin/delete/sgrmbroker.bat
@@ -0,0 +1,14 @@
+@echo off
+
+
+set mount_path="%~1"
+
+if %mount_path% == "" (
+    echo.
+    echo   [*ERROR] Mount path argument required
+    echo.
+    pause
+    exit /b 1
+)
+
+del /s /q %mount_path:~0,-1%\Windows\System32\SgrmBroker.exe" > nul 2>&1
